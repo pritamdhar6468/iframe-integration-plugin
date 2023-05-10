f89f5a9934fb68f9a95f63e2cd1fc0d16aab1e11
Pritam Dhar <84518701+pritamdhar6468@users.noreply.github.com>
2023-05-10 19:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Define the parameter name
 const paramName = 'iframe_url';
 
-// Get the value of the parameter from the URL
+
 const urlParams = new URLSearchParams(window.location.search);
 const iframeUrl = urlParams.get(paramName);
 
@@ -12,7 +12,7 @@ if (iframeUrl) {
   iframe.setAttribute('style', 'width: 100%; height: 500px;');
   document.body.appendChild(iframe);
 
-  // Create a hover button on top of the iFrame
+ 
   const button = document.createElement('button');
   button.textContent = 'Click me';
   button.setAttribute('style', 'position: absolute; color: black top: 10px; right: 10px; z-index: 999;');
